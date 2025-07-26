c2c9b20e6b8a991ea3d2e47f97f06807f563c269
Martin Pasiecznik <104079011+Martin-Pasiecznik@users.noreply.github.com>
2025-07-26 18:40:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 // Podés agregar funciones interactivas más adelante
-console.log("Página cargada correctamente.");
+console.log("OPRIMISTE F12? CERRA ESTA PAGINA VOLVIENDO A PRESIONAR F12. SALUDOS!!!");
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.innerWidth <= 768) {
+        const sections = document.querySelectorAll("section");
+
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add("active");
+                    } else {
+                        entry.target.classList.remove("active");
+                    }
+                });
+            },
+            {
+                threshold: 0.5 // Se activa cuando el 50% de la sección está visible
+            }
+        );
+
+        sections.forEach((section) => {
+            observer.observe(section);
+        });
+    }
+});
